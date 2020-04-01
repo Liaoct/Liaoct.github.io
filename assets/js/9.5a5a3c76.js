43fab9477c6dad2c4f809e37222b18e45f9e2393
Travis CI User <travis@example.org>
2020-04-01 05:49:46 +0000

    deploy

---
+++ assets/js/9.5a5a3c76.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{278:function(t,n,s){"use strict";s.r(n);var a={name:"CustomLayout"},e=s(0),i=Object(e.a)(a,function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"custom-layout"},[n("div",{staticClass:"main-div"},[n("Content")],1)])},[],!1,null,null,null);n.default=i.exports}}]);
